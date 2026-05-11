693628bf0e5396528b1348ab95e0b91fd7a84700
madina0511 <madinkashakirovna@gmail.com>
2026-05-12 07:46:00 +0900

    Implement Excel upload endpoint

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import * as XLSX from 'xlsx';
 
 @Controller('upload')
-export class UploadController {}
+export class UploadController {
+  @Post('excel')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadExcel(@UploadedFile() file: any) {
+    const workbook = XLSX.read(file.buffer, { type: 'buffer' });
+
+    const sheetName = workbook.SheetNames[0];
+
+    const worksheet = workbook.Sheets[sheetName];
+
+    const data = XLSX.utils.sheet_to_json(worksheet);
+
+    console.log(data);
+
+    return {
+      message: 'Excel uploaded successfully',
+      data,
+    };
+  }
+}
